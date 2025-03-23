4f4e65e5262d31cf3945e1d0e29ee4dc61f26ba4
alijardariasif123 <alijardariasif123@gmail.com>
2025-03-23 23:42:20 +0530

    webhooks added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,65 @@
-
+import { Webhook } from "svix"
+import userModel from "../models/userModel.js"
 //API controller function to manage clerk user with database
 // http://localhost:4000/api/user/webhooks
 
 const clerkWebhooks = async (req, res) => {
+    try {
+        //create a svix instance with clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+        
+        const {data, type} = req.body
+        switch (type) {
+            case "user.created":{
+                const userdata = {
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    phoneNo: data.phone_numbers[0].phone_number,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+
+                await userModel.create(userdata)
+                res.json({})
+
+                break;
+            }
+            case "user.updated":{
+                const userdata = {
+                    email: data.email_addresses[0].email_address,
+                    phoneNo: data.phone_numbers[0].phone_number,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+
+                await userModel.findOneAndUpdate({clerkId: data.id}, userdata)
+                res.json({})
+
+                break;
+            }
+            case "user.deleted":{
+                await userModel.findOneAndDelete({clerkId: data.id})
+                res.json({})
+
+                break;
+            }               
+        
+            default:
+                break;
+        }
+
+    } catch (error) {
+        console.log(error.mesage) //handle error
+        res.json({success:false, message: error.message})
+    }
     
 }
+
+export {clerkWebhooks}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 //App config
 const port = process.env.PORT || 4000;
@@ -14,7 +15,7 @@ app.use(express.json());
 
 //Api routes
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.use('/api/user',userRouter)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
