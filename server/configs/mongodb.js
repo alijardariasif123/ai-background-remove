06cf1316f17bbf33896fd3d19889b7f400d449b5
alijardariasif123 <alijardariasif123@gmail.com>
2025-03-24 15:24:24 +0530

    vvv

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
+
 const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("MongoDB connected");
   })
 
-  await mongoose.connect(process.env.MONGODB_URI, {
-    dbName: "bg-removal",  // Database Name Set करें
-});
+  await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
 
 }
 export default connectDB;
